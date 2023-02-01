1525d13ba6a48418d7570a8eb697b237fd21b0d3
Luwafemi <aladefemi1@gmail.com>
2023-02-01 10:21:29 +0100

    Map out tasks from a fake REST API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
-import Header from "./components/Header"
+import React, { useEffect } from "react";
+import {
+  FaRegTrashAlt,
+  FaRegBell,
+  FaRegCircle,
+  FaRegCheckCircle,
+} from "react-icons/fa";
+
 function App() {
+  const [tasks, setTasks] = React.useState([]);
+  React.useEffect(() => {
+    async function getTasksFromServer() {
+      var response = await fetch("http://localhost:5000/tasks");
+      response = await response.json();
+      setTasks(response);
+    }
+    getTasksFromServer();
+  }, []);
+  // console.log("ok");
+
   return (
-    <div>
-      <Header/>
+    <div className="container">
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row-reverse",
+        }}
+      >
+        <button className="btn btn-primary mt-2"> Add Task </button>
+      </div>
+      <h1 style={{ marginBottom: 50, marginTop: 30, fontWeight: 800 }}>
+        {" "}
+        TASKS{" "}
+      </h1>
+      <ul style={{ listStyle: "none" }}>
+        {tasks.map((task) => (
+          <div key={task.id} style={{ display: "flex", marginBottom: 20 }}>
+            <FaRegCircle />
+            <span
+              style={{ display: "flex", marginRight: "20vw", marginLeft: 10 }}
+            >
+              <li style={{ marginTop: -4 }}>
+                {task.taskName}
+              </li>
+            </span>
+            <FaRegBell />
+            <span style={{ width: 20 }}></span>
+            <FaRegTrashAlt />
+            {/* <FaRegCheckCircle /> */}
+          </div>
+        ))}
+      </ul>
+      <footer style={{ position: "absolute", bottom: 10, left: "40%" }}>
+        <div>'Luwafemi A. &copy;{new Date().getFullYear()}</div>
+      </footer>
     </div>
   );
-}
+  
+  }
+
 
 export default App;
